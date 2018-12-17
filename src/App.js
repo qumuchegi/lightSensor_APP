b5ad18e6eda3905a7404c73ecfb5f3bfcadfec1d
曲木车格 <qumuchege@qumuchegedeMacBook-Pro.local>
2018-12-17 22:06:27 +0800

    butify the style

--- src/App.js
+++ src/App.js
@@ -1,7 +1,6 @@
 import React, { Component } from 'react';
 import './App.css';
 const ws = new WebSocket('ws://chegi.xyz:80')
-//const socket = client('ws://chegi.xyz:9091')
 
 class App extends Component {
   constructor(props){
@@ -30,34 +29,30 @@ class App extends Component {
       }
   }
   on(){
-
-    //socket.emit('send',{light:'2'})///
-    //ws.send(JSON.stringify({light:'8'}))
-    //console.log(ws.readyState)
     if(ws.readyState===1)
     ws.send(0)
-    //ws.onmessage=data=>this.receiveOn(data)
+    
   }
   off(){
     ws.send(1)
-    //ws.onmessage=data=>this.receiveOn(data)
    }
   render() {
     return (
       <div className="App">
         <div>
           <p>物联网远程控灯</p>
+          <h3>实时显示亮度和控制LED</h3>
         </div>
  
         <div id="result">
           <h4>操作结果</h4>
-          {
-            this.state.haslighton?<h3 id='on'>已经点亮了！！</h3>:<h3 id='off'>已经熄灭！！</h3>
-          }
-          <div id="light">亮度：{this.state.light}</div>
+          <div id="light">亮度：
+          <span id="number">{this.state.light}</span>
+          Lux
+          </div>
         </div>  
      
-        <div>
+        <div id="button-container">
           <div id='button' onClick={()=>this.on()}>点亮</div>
           <div id='button' onClick={()=>this.off()}>熄灭</div> 
         </div>
